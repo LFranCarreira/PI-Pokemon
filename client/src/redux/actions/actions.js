8fb4a8cb9d8e27d8fd43057392dd733d4d873ed1
Franco Carreira <carreirafranco@gmail.com>
2023-08-10 22:41:57 -0300

    ready to deploy

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -5,7 +5,7 @@ import { GET_POKEMONS } from "./types";
 export const getPokemons = () => {
   return async (dispatch) => {
       try {
-          const URL = 'http://localhost:3001/pokemons';
+          const URL = '/pokemons';
           const pk = await axios.get(URL);
           return dispatch({
               type: GET_POKEMONS,

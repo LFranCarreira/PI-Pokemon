d930bffd7abd4775a4280eff1be6b6ca162c1470
Franco Carreira <carreirafranco@gmail.com>
2023-09-11 20:20:17 -0300

    error

--- api/src/app.js
+++ api/src/app.js
@@ -13,7 +13,6 @@ server.name = 'API';
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use(cors())
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from

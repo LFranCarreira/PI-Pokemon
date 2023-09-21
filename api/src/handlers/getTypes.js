944c32c6908e9b337a2faad7782e34d4b54998fb
Franco Carreira <carreirafranco@gmail.com>
2023-09-21 01:00:50 -0300

    add whitespaces

--- api/src/handlers/getTypes.js
+++ api/src/handlers/getTypes.js
@@ -1,13 +1,13 @@
-const { getTypes } = require("../controllers/allTypes");
-const STATUS_ERROR=404;
-const STATUS_OK=200
+const { getTypes } = require('../controllers/allTypes');
+const STATUS_ERROR = 404;
+const STATUS_OK = 200;
 const getAllTypes = async (req, res) => {
-    //it gets the types or catches an error
-    try {
-        const types = await getTypes()
-        return res.status(STATUS_OK).send(types);
-    } catch (error) {
-        res.status(STATUS_ERROR).json({ error: error.message });
-    }
+  //it gets the types or catches an error
+  try {
+    const types = await getTypes();
+    return res.status(STATUS_OK).send(types);
+  } catch (error) {
+    res.status(STATUS_ERROR).json({ error: error.message });
+  }
 };
-module.exports =  {getAllTypes} ;
+module.exports = { getAllTypes };

40eacd87645d69d03026879342d37d4bf64ba1e5
Franco Carreira <carreirafranco@gmail.com>
2023-09-21 00:51:29 -0300

    add whitespaces

--- api/src/handlers/getPokemonId.js
+++ api/src/handlers/getPokemonId.js
@@ -1,16 +1,15 @@
-const { getPokemonById }=require("../controllers/getPokemonById");
-const STATUS_ERROR=404
-const STATUS_OK=200
-const getPokemonId = async (req, res) => {  
-  const {idPokemon}=req.params
+const { getPokemonById } = require('../controllers/getPokemonById');
+const STATUS_ERROR = 404;
+const STATUS_OK = 200;
+const getPokemonId = async (req, res) => {
+  const { idPokemon } = req.params;
   //recieves the id by params and search the pokemon by id from the DB and API
-  try{
+  try {
     const pk = await getPokemonById(idPokemon);
     res.status(STATUS_OK).json(pk);
-  }catch(error){
-    res.status(STATUS_ERROR).json({error:error.message})
+  } catch (error) {
+    res.status(STATUS_ERROR).json({ error: error.message });
   }
-}
+};
 
-
-module.exports={getPokemonId}
+module.exports = { getPokemonId };

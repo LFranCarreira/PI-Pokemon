02f9d899910977f86e6b400a7b9e638002eaf449
Franco Carreira <carreirafranco@gmail.com>
2023-09-21 00:50:48 -0300

    add whitespace

--- api/src/controllers/newPokemon.js
+++ api/src/controllers/newPokemon.js
@@ -1,7 +1,8 @@
-const {Pokemons,Type}=require("../db");
+const { Pokemons, Type } = require('../db');
 
-const newPokemon = async (pokemonData)=>{
-  const { Name, Image, Health, Attack, Defense, Speed, Height, Weight, Types } = pokemonData
+const newPokemon = async (pokemonData) => {
+  const { Name, Image, Health, Attack, Defense, Speed, Height, Weight, Types } =
+    pokemonData;
   // Recieve the info to create the pokemon
   const existingTypes = await Type.findAll({
     where: {
@@ -15,22 +16,25 @@ const newPokemon = async (pokemonData)=>{
   });
   // Verify if the types exists
   if (existingTypes.length !== Types.length) {
-    const nonExistentTypes = Types.filter((type) => !existingTypes.some((existingType) => existingType.Name === type));
+    const nonExistentTypes = Types.filter(
+      (type) =>
+        !existingTypes.some((existingType) => existingType.Name === type)
+    );
     throw new Error(`You can not repeat the same type`);
   }
 
-  const url = "https://pokeapi.co/api/v2/pokemon/" + Name;
+  const url = 'https://pokeapi.co/api/v2/pokemon/' + Name;
   //we try to search the pokemon if it already exists
   await fetch(url)
     .then((response) => response.json())
     .then((pokemon) => {
-      results = pokemon
+      results = pokemon;
     })
-      .catch((error) => {
-       results = error
-      });
+    .catch((error) => {
+      results = error;
+    });
   //if the id exists, throw error
-  if (results["id"]) throw new Error("That name belongs to another Pokemon");
+  if (results['id']) throw new Error('That name belongs to another Pokemon');
   const newPokemon = await Pokemons.create({
     Name,
     Image,
@@ -43,11 +47,11 @@ const newPokemon = async (pokemonData)=>{
   });
   //create the new pokemon
 
-    for (const type of existingTypes) {
-      await newPokemon.addType(type);
-    }
-    //we add the types and return the pokemon
+  for (const type of existingTypes) {
+    await newPokemon.addType(type);
+  }
+  //we add the types and return the pokemon
   return newPokemon;
 };
 
-module.exports = {newPokemon};
+module.exports = { newPokemon };

0c75f72bc4036f8b840edae94936088fa64a9c4c
Franco Carreira <carreirafranco@gmail.com>
2023-09-21 01:01:09 -0300

    add whitespace

--- api/src/handlers/postPokemon.js
+++ api/src/handlers/postPokemon.js
@@ -1,14 +1,14 @@
-const {newPokemon}=require("../controllers/newPokemon")
-const STATUS_OK=200;
-const STATUS_ERROR=404;
+const { newPokemon } = require('../controllers/newPokemon');
+const STATUS_OK = 200;
+const STATUS_ERROR = 404;
 const postPokemon = async (req, res) => {
   //it tries to post the pokemon,otherwise it catches an error
   try {
     await newPokemon(req.body);
-    res.status(STATUS_OK).json("Pokemon created succesfully");
+    res.status(STATUS_OK).json('Pokemon created succesfully');
   } catch (error) {
     res.status(STATUS_ERROR).json({ error: error.message });
   }
 };
-  
-  module.exports = {postPokemon};
+
+module.exports = { postPokemon };

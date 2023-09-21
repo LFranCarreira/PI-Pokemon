f6508998b83c89a06233dcbe78f6c4deedae42b9
Franco Carreira <carreirafranco@gmail.com>
2023-09-21 01:01:47 -0300

    add whitespace

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -3,45 +3,49 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Pokemons', {
-    ID: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull:false,
-      primaryKey:true
+  sequelize.define(
+    'Pokemons',
+    {
+      ID: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      Name: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      Image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      Health: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      Attack: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      Defense: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      Speed: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      Height: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+      Weight: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
     },
-    Name: {
-      type: DataTypes.STRING,
-      unique:true,
-      allowNull:false
-    },
-    Image: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    Health: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    Attack: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    Defense: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    Speed: {
-      type: DataTypes.INTEGER,
-      allowNull:true
-    },
-    Height: {
-      type: DataTypes.FLOAT,
-      allowNull:true
-    },
-    Weight:{
-      type:DataTypes.INTEGER,
-      allowNull:true
-    }
-  }, {timestamps: false} );
+    { timestamps: false }
+  );
 };

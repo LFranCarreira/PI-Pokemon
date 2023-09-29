136a9087db1c8e80d2b0dc7e6a947c571e38c124
Franco Carreira <carreirafranco@gmail.com>
2023-09-29 18:11:14 -0300

    add whitespaces

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -1,11 +1,11 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const router = Router();
-const { getPokemonId }=require("../handlers/getPokemonId")
-const { postPokemon } = require("../handlers/postPokemon");
-const { getAllPokemons } = require("../handlers/getPokemons");
+const { getPokemonId } = require('../handlers/getPokemonId');
+const { postPokemon } = require('../handlers/postPokemon');
+const { getAllPokemons } = require('../handlers/getPokemons');
 
-router.get("/pokemons",getAllPokemons)
-router.get("/pokemons/:idPokemon",getPokemonId)
-router.post("/pokemons",postPokemon)
+router.get('/pokemons', getAllPokemons);
+router.get('/pokemons/:idPokemon', getPokemonId);
+router.post('/pokemons', postPokemon);
 
 module.exports = router;

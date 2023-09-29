af303066da70c1ddcfaeba9873bac1d843a09119
Franco Carreira <carreirafranco@gmail.com>
2023-09-29 18:14:15 -0300

    pokemon test

--- api/tests/routes/pokemonName.spec.js
+++ api/tests/routes/pokemonName.spec.js
@@ -1,21 +1,19 @@
 const request = require('supertest');
 const { expect } = require('chai');
-const app = require('../../src/app.js'); 
+const app = require('../../src/app.js');
 describe('GET /pokemons', () => {
-    it('should respond with status 200 and data when a valid Pokemon name is provided', (done) => {
-      request(app)
-        .get('/pokemons/pikachu')
-        .expect(200)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.body).to.be.an('object');
-          done();
-        });
-    });
-  
-    it('should respond with status 404 when an invalid Pokemon name is provided', (done) => {
-      request(app)
-        .get('/pokemons/tatata')
-        .expect(404, done);
-    });
+  it('should respond with status 200 and data when a valid Pokemon name is provided', (done) => {
+    request(app)
+      .get('/pokemons/pikachu')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).to.be.an('object');
+        done();
+      });
   });
+
+  it('should respond with status 404 when an invalid Pokemon name is provided', (done) => {
+    request(app).get('/pokemons/tatata').expect(404, done);
+  });
+});

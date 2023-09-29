2e0a76f0a9eda0450bf2d5ea20b4b3dbf1946050
Franco Carreira <carreirafranco@gmail.com>
2023-09-29 18:18:40 -0300

    action

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,18 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
 
-import { GET_POKEMONS } from "./types";
+import { GET_POKEMONS } from './types';
 //getPokemons, it gets the pokemons created in the db and the first 100 from the api that are found in the ulr
 export const getPokemons = () => {
   return async (dispatch) => {
-      try {
-          const URL = 'http://localhost:3001/pokemons';
-          const pk = await axios.get(URL);
-          return dispatch({
-              type: GET_POKEMONS,
-              payload: pk.data
-          });
-      } catch (error) {
-          console.log(error);
-      };     
+    try {
+      const URL = 'http://localhost:3001/pokemons';
+      const pk = await axios.get(URL);
+      return dispatch({
+        type: GET_POKEMONS,
+        payload: pk.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
